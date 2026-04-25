461f439dbb09229766dbb3ef1da3e03e05bfe979
Mikael <mikaelc623@gmail.com>
2026-04-25 18:28:38 -0300

    feat: conflito na leitura do .env resolvido

--- backend/src/config/firebase.py
+++ backend/src/config/firebase.py
@@ -1,20 +1,13 @@
 import os
 import firebase_admin
-from dotenv import load_dotenv
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 
-# Carrega variáveis do .env
 load_dotenv()
 
-# Pega o caminho da credencial do .env
-cred_path = os.getenv("FIREBASE_CREDENTIALS")
-
-if not cred_path:
-    raise ValueError("FIREBASE_CREDENTIALS não definido no .env")
-
-# Inicializa Firebase com a credencial
-cred = credentials.Certificate(cred_path)
+path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+print("PATH:", path)
+cred = credentials.Certificate(path)
 firebase_admin.initialize_app(cred)
 
-# Cliente do Firestore
 db = firestore.client()
