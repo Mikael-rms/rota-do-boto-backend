ec7bf2eceddac3ba2b40620fc55dc4b1be6e3343
Mikael <mikaelc623@gmail.com>
2026-05-10 14:43:48 -0300

    fix cors

--- firebase_config.py
+++ firebase_config.py
@@ -1,26 +1,8 @@
-import os
-import json
-
-from dotenv import load_dotenv
-
 import firebase_admin
+from firebase_admin import credentials, firestore
 
-from firebase_admin import credentials
-from firebase_admin import firestore
+cred = credentials.Certificate("firebasekey.json")
 
-
-load_dotenv()
-
-firebase_key = os.getenv("FIREBASE_KEY")
-
-if not firebase_key:
-    raise ValueError("FIREBASE_KEY not found")
-
-firebase_dict = json.loads(firebase_key)
-
-cred = credentials.Certificate(firebase_dict)
-
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
